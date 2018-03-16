fa59c5cd4ce0cf1347ceda4e320ada678d072b2a
hris11 <hris11@abv.bg>
2018-03-16 16:22:46 +0200

    Socket connection

--- board-game/ws.js
+++ board-game/ws.js
@@ -2,12 +2,18 @@ var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({port: 40510})
 
 wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  });
+    
 
-  setInterval(
-    () => ws.send(`${new Date()}`),
+
+    ws.on('message', function (message) {
+    console.log('received: %s', message)
+    });
+
+    setInterval(
+    () => ws.send(JSON.stringify({
+        type: "dice",
+        value: `${Math.round(Math.random() * 5) + 1}`
+    })),
     1000
-  )
+    )
 });
