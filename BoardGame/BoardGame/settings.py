41cb8990822a860d8e542f883e2c5d65bd051e90
Slavkata <slavkirilov00@gmail.com>
2018-03-30 21:58:38 +0300

    adding access for all machines on a local network

--- BoardGame/BoardGame/settings.py
+++ BoardGame/BoardGame/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z5qsap#k@2!tt_#h^$8+(x7v2)3gk+#4lvh1&)xab&&02r-ntl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
